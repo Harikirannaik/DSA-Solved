331b44df5e28466a97f6ba84c42aa95543294070
Harikirannaik <126312415+Harikirannaik@users.noreply.github.com>
2023-12-28 15:07:09 +0530

    Update AddTwoNum.java

--- AddTwoNum.java
+++ AddTwoNum.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class AddTwoNum{
     public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        int n1 = s.nextInt();
-        int n2 = s.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
         int sum = n1 + n2 ;
         System.out.println("Sum is: " + sum);
     }
