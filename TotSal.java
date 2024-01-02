5ab98f6b438f7145aa78b0c4a24f29fa2ce54a4b
Harikirannaik <126312415+Harikirannaik@users.noreply.github.com>
2024-01-02 06:42:35 +0530

    new file added

---
+++ TotSal.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class TotSal {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int basic = s.nextInt();
+        char grade = s.next().charAt(0);
+
+        double hra = 0.2 * basic;
+        double da = 0.5 * basic;
+        int allow = 0;
+        double pf = 0.11 * basic;
+
+        if (grade == 'A'){
+            allow = 1700;
+        }
+        else if (grade == 'B') {
+            allow = 1500;
+        }
+        else{
+            allow = 1300;
+        }
+
+        double totsal = basic + hra + da + allow - pf ;
+        int roundtotsal = (int) Math.round(totsal);
+        System.out.println(roundtotsal);
+
+    }
+}
